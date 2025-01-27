ba25b86ace173c8df4acf8dbf57ad8e98b50f8cb
Роман Беспалов <bespalovroma1995@gmail.com>
2025-01-27 19:28:29 +0300

    moved redis connection check to a separate function

--- backend/matrix/constants.py
+++ backend/matrix/constants.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 
+
 CURRENT_DATE = timezone.now().date()
 CURRENT_MONTH = timezone.now().month
 JOB_TITLE_USERS = [
--- backend/matrix/tasks.py
+++ backend/matrix/tasks.py
@@ -2,6 +2,7 @@ import os
 from celery import shared_task
 from tempfile import NamedTemporaryFile
 from openpyxl import Workbook
+from django.shortcuts import get_object_or_404
 
 from matrix.models import Competence, GradeSkill, Skill, User
 from matrix.constants import CURRENT_MONTH
@@ -16,8 +17,8 @@ def save_to_db(data, user_id):
     ).delete()
 
     for skill, grade in data.items():
-        new_skill = Skill.objects.get(skill=skill)
-        grade_skill = GradeSkill.objects.get(grade=grade[0])
+        new_skill = get_object_or_404(Skill, skill=skill)
+        grade_skill = get_object_or_404(GradeSkill, grade=grade[0])
         Competence.objects.create(
             user=user,
             skill=new_skill,
